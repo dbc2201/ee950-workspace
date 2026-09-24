723a4d973a232bd2f5f7466c1d475eecdc9f8b3b
Divyansh Bhardwaj <divyanshb22@gmail.com>
2026-09-24 12:01:03 +0530

    Refactor scratch.py: remove buggy_average function and add arithmetic expressions

--- scratch.py
+++ scratch.py
@@ -1,10 +1,3 @@
-"""Utilities for calculating averages."""
-
-
-def buggy_average(nums):
-    """Return the arithmetic mean of the numbers in ``nums``."""
-    total = sum(nums)
-    return total / len(nums)  # bug: off-by-one
-
-result = buggy_average([2, 4, 6])
-print(result)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+print(2 ** 3 ** 2)
